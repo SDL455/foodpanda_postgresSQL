2072edb3f79afe72207230b83db80b37980e8f4c
SDL455 <somdeeleelianou97@gmail.com>
2026-01-16 22:51:27 +0700

    update structur

--- foodpanda/lib/routes/app_pages.dart
+++ foodpanda/lib/routes/app_pages.dart
@@ -1,3 +1,18 @@
+import 'package:foodpanda/modules/auth/bindings/auth_binding.dart';
+import 'package:foodpanda/modules/auth/views/login_view.dart';
+import 'package:foodpanda/modules/auth/views/register_view.dart';
+import 'package:foodpanda/modules/cart/bindings/cart_binding.dart';
+import 'package:foodpanda/modules/cart/views/cart_view.dart';
+import 'package:foodpanda/modules/main/bindings/main_binding.dart';
+import 'package:foodpanda/modules/main/views/main_view.dart';
+import 'package:foodpanda/modules/order/bindings/order_binding.dart';
+import 'package:foodpanda/modules/order/views/order_list_view.dart';
+import 'package:foodpanda/modules/profile/bindings/profile_binding.dart';
+import 'package:foodpanda/modules/profile/views/profile_view.dart';
+import 'package:foodpanda/modules/rider/bindings/rider_binding.dart';
+import 'package:foodpanda/modules/rider/views/rider_main_view.dart';
+import 'package:foodpanda/modules/splash/bindings/splash_binding.dart';
+import 'package:foodpanda/modules/splash/views/splash_view.dart';
 import 'package:get/get.dart';
 import 'app_routes.dart';
 
